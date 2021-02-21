06b2bdb113110109a3bd56bec2751f536b41edbc
Lucas Norberto <77136264+lucasnorberto@users.noreply.github.com>
2021-02-21 10:42:36 -0300

    update css

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-/*function fecharDialog(){
-    document.getElementById("btn_dialog").innerHTML="&lt;!--...--&gt;";
-    document.getElementById("tagcoment").removeAttribute("open");
-}
 
-function abrirDialog(){
-    document.getElementById("btn_dialog").innerHTML="&lt;!--...--&gt;";
-    document.getElementById("taccoment").setAttribute("open","");
-}
-
-function controlaDialog(){
-    if(document.getElementById("tagcoment").open){
-        fecharDialog();
-    }else { 
-        abrirDialog();      
-      }
-
-    }
-    */
 
     function fecharDialog(){
         document.getElementById("btn_dialog").innerHTML="&lt;!--...--&gt;";
@@ -26,10 +8,13 @@ function controlaDialog(){
         document.getElementById("btn_dialog").innerHTML="&lt;!--...--&gt;";
         document.getElementById("d1").setAttribute("open","");
     }
-    function controlaDialog(){
+    function controlaDialog(titulo,msg){
         if(document.getElementById("d1").open){
             fecharDialog();
         }else{
             abrirDialog();
+            document.getElementById("titulo").innerHTML=titulo;
+            document.getElementById("msg1").innerHTML=msg
+            
         }
     }
